d6977e039d26bdf1ae5284082baa8e27bfd7120a
Salim <salimgizat@mail.ru>
2022-09-01 18:27:17 +0300

    DONE

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -12,32 +12,32 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDao userDaoJDBC = new UserDaoJDBCImpl();
-    //UserDao userDaoHib = new UserDaoHibernateImpl();
+    //UserDao userDaoJDBC = new UserDaoJDBCImpl();
+    UserDao userDaoHib = new UserDaoHibernateImpl();
 
 
     public void createUsersTable() throws SQLException {
-        userDaoJDBC.createUsersTable();
+        userDaoHib.createUsersTable();
     }
 
     public void dropUsersTable() throws SQLException {
-        userDaoJDBC.dropUsersTable();
+        userDaoHib.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        userDaoJDBC.saveUser(name, lastName, age);
+        userDaoHib.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) throws SQLException {
-        userDaoJDBC.removeUserById(id);
+        userDaoHib.removeUserById(id);
 
     }
 
     public List<User> getAllUsers() throws SQLException {
-        return userDaoJDBC.getAllUsers();
+        return userDaoHib.getAllUsers();
     }
 
     public void cleanUsersTable() throws SQLException {
-        userDaoJDBC.cleanUsersTable();
+        userDaoHib.cleanUsersTable();
     }
 }
